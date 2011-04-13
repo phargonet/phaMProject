71905f7127b7c70e56fec3c05a1cea70f6af23ec
Vadim <long@phargo.net>
2011-04-14 02:33:10 +0400

    add transition for dialog

---
+++ jqconst/MTransition.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * MTransition class file.
+ *
+ * @author Vadim Kruchkov <long@phargo.net>
+ * @link https://github.com/phargo/phaMProject
+ * @copyright Copyright &copy; 2011 PhArgo.net
+ * @license Dual licensed under the MIT or GPL Version 2 licenses:
+ *      - https://github.com/phargo/phaMProject/blob/master/MIT-LICENSE
+ *      - https://github.com/phargo/phaMProject/blob/master/GPL-LICENSE
+ * @package phaMProject
+ * @subpackage jQConstant
+ */
+
+/**
+ * MTransition defines constants for style opening dialogs in jQuery Mobile
+ *
+ * @author Vadim Kruchkov <long@phargo.net>
+ * @package phaMProject
+ * @subpackage jQConstant
+ */
+class MTransition {
+
+    /**
+     * Defines constant for style openin "pop"
+     * @see http://jquerymobile.com/demos/1.0a4.1/docs/pages/docs-dialogs.html
+     *
+     * @var string
+     */
+    const POP = 'pop';
+
+    /**
+     * Defines constant for style openin "slideup"
+     * @see http://jquerymobile.com/demos/1.0a4.1/docs/pages/docs-dialogs.html
+     *
+     * @var string
+     */
+    const SLIDEUP = 'slideup';
+
+    /**
+     * Defines constant for style openin "slideup"
+     * @see http://jquerymobile.com/demos/1.0a4.1/docs/pages/docs-dialogs.html
+     *
+     * @var string
+     */
+    const FLIP = 'flip';
+}
