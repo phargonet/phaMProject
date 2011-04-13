d4829bef64aab4226232164bc9c621cc2d5d5903
Vadim <long@phargo.net>
2011-04-14 03:04:00 +0400

    add elements theme

---
+++ jqconst/MTheme.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * MTheme class file.
+ *
+ * @author Vadim Kruchkov <long@phargo.net>
+ * @link https://github.com/phargo/phaMProject
+ * @copyright Copyright &copy; 2011 PhArgo.net
+ * @license Dual licensed under the MIT or GPL Version 2 licenses:
+ *      - https://github.com/phargo/phaMProject/blob/master/MIT-LICENSE
+ *      - https://github.com/phargo/phaMProject/blob/master/GPL-LICENSE
+ * @package phaMProject
+ * @subpackage jQConstant
+ */
+
+/**
+ * MTheme defines constants for existing teams in jQuery Mobile
+ *
+ * @author Vadim Kruchkov <long@phargo.net>
+ * @package phaMProject
+ * @subpackage jQConstant
+ */
+class MTheme {
+    /**
+     * Theme by default.
+     * The default Theme mixes styles from multiple swatches to create visual texture
+     * and present the various elements in optimal contrast to one anothe
+     *
+     * @var string
+     */
+    const BY_DEFAULT = '';
+
+    /**
+     * Defines constant for "Dark" theme
+     *
+     * @var string
+     */
+    const DARK = 'a';
+
+    /**
+     * Defines constant for "Blue" theme
+     *
+     * @var string
+     */
+    const BLUE = 'b';
+
+    /**
+     * Defines constant for "Gray" theme
+     *
+     * @var string
+     */
+    const GRAY = 'c';
+
+    /**
+     * Defines constant for "White" theme
+     *
+     * @var string
+     */
+    const WHITE = 'd';
+
+    /**
+     * Defines constant for "Yellow" theme
+     *
+     * @var string
+     */
+    const YELLOW = 'e';
+}
